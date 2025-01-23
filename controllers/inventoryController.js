1080869213379d0c2ef75d1ae24dcbaf6f89d48b
Igberry <davidigberichuks@gmail.com>
2025-01-23 23:38:13 +0100

    commit

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,5 +1,19 @@
 const inventoryModel = require('../models/inventory-model');
 const utilities = require('../utilities');
+const nav = `
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/inventory">Inventory</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+`;
+
+res.render('layouts/layout', {
+  title: 'Page Title',
+  body: 'Body Content',
+  nav, // Pass the nav variable here
+});
+
 
 exports.getVehicleDetails = async (req, res, next) => {
   try {
